89ef7c85918aec8c5ea3395c171d794c21171b5c
Dwight Spencer <toketeeman@comcast.net>
2018-01-04 16:07:06 -0800

    Parent-to-child communication added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,31 @@
+import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
-
+import { NavComponent } from './nav/nav.component';
+import { AuthService } from './_services/auth.service';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ValueComponent
+	declarations: [
+		AppComponent,
+		ValueComponent,
+		NavComponent,
+    HomeComponent,
+    RegisterComponent
 ],
-  imports: [
-    BrowserModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	imports: [
+		BrowserModule,
+		HttpModule,
+		FormsModule
+	],
+	providers: [
+		AuthService
+	],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
