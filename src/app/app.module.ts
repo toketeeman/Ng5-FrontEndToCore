f180d5eba39f107b6a901d31d5afb627b24d14d1
Dwight Spencer <toketeeman@comcast.net>
2018-01-09 13:24:48 -0800

    Adding routing mods.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BsDropdownModule } from 'ngx-bootstrap';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
@@ -12,19 +13,27 @@ import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { AlertifyService } from './_services/alertify.service';
 import { TitleCasePipe } from '@angular/common';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { appRoutes } from './routes';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    ListsComponent,
+    MessagesComponent
 ],
 	imports: [
 		BrowserModule,
 		HttpModule,
 		FormsModule,
-		BsDropdownModule.forRoot()
+		BsDropdownModule.forRoot(),
+		RouterModule.forRoot(appRoutes)
 	],
 	providers: [
 		AuthService,
