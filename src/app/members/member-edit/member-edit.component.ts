3cf6b7ccaddca00f617bab6b8d76aa7623353641
Dwight Spencer <toketeeman@comcast.net>
2018-01-20 14:18:34 -0800

    Set up profile form.

--- src/app/members/member-edit/member-edit.component.ts
+++ src/app/members/member-edit/member-edit.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 import { User } from '../../_models/User';
 import { ActivatedRoute } from '@angular/router';
+import { AlertifyService } from '../../_services/alertify.service';
 
 
 @Component({
@@ -11,13 +13,20 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MemberEditComponent implements OnInit {
   user: User;
+  @ViewChild('editForm') childEditForm: NgForm;   // Need access to form to reset it upon submit.
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private alertify: AlertifyService) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {   // Grab the resolved user here.
       this.user = data['resolvedUser'];
     })
+  }
+
+  updateUser() {
+    console.log(this.user);
+    this.alertify.success('Profile Updated Successfully');
+    this.childEditForm.reset(this.user);
   }
 
 }
